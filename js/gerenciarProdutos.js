81ff14da0c95002e4f12c3c74bd91b5eca1a5fe7
Miguel Broetto <miguelperinibroetto@gmail.com>
2026-06-24 11:54:05 -0300

    implementando loocalStorage

--- js/gerenciarProdutos.js
+++ js/gerenciarProdutos.js
@@ -104,12 +104,25 @@ function cadastrarProduto() {
     } else {
         vetProdutos.push(produto);
 
-        console.log(vetProdutos);
+        const jsonVetProdutos = JSON.stringify(vetProdutos);
+        console.log(jsonVetProdutos);
+
+        localStorage.setItem("listaprodutos", jsonVetProdutos);
 
         alert("Produto cadastrado!");
 
         limparCampos();
     }
+}
+
+function armazanarDados(produto) {
+
+    let vetProdutos = [];
+    vetProdutos.push(produto);
+    let jsonVetProdutos = JSON.stringify(vetProdutos);
+    localStorage.setItem("listaprodutos", jsonVetProdutos);
+
+    //localStorage.setItem("produtos", JSON.stringify([produto]));
 }
 
 function mostrarProdutos() {
@@ -223,7 +236,7 @@ function filtrarDisponibilidade() {
                     status = "disponivel";
                 } else if (vetProdutos[ind].estoque >= 1 && vetProdutos[ind].estoque <= 10) {
                     status = "baixo";
-                } else if(vetProdutos[ind].estoque <=0){
+                } else if (vetProdutos[ind].estoque <= 0) {
                     status = "esgotado";
                 }
 
